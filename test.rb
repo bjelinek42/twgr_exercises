83e5a87aa661708fcb19ef6e812302772195af40
ben <bjelinek42@gmail.com>
2022-08-14 19:28:32 -0600

    ch 4 exercises

---
+++ test.rb
@@ -0,0 +1,9 @@
+# c = Class.new do
+#   def hello
+#     "hello"
+#   end
+# end
+
+# d = c.new
+# p d.hello
+
--- ticket.rb
+++ ticket.rb
@@ -1,7 +1,9 @@
 class Ticket
+  attr_accessor :price
 
-  def initialize(venue)
+  def initialize(venue, date)
     @venue = venue
+    @date = date
   end
 
   def price=(amount)
@@ -14,19 +16,32 @@ class Ticket
     p @discount
   end
 
-  def date=(date)
-    @date = date
-    validate = date.split("-")
-    p validate
-    if validate[0].length == 4 && validate[1].length == 2 && validate[2].length == 2 && validate[1].to_i <= 12
-      p date
-    else
-      p "Please submit the date in the format 'yyyy-mm-dd'"
-    end
+  def Ticket.most_expensive(*tickets)
+    tickets.max_by(&:price)
   end
+
+  # def date=(date)
+  #   @date = date
+  #   validate = date.split("-")
+  #   p validate
+  #   if validate[0].length == 4 && validate[1].length == 2 && validate[2].length == 2 && validate[1].to_i <= 12
+  #     p date
+  #   else
+  #     p "Please submit the date in the format 'yyyy-mm-dd'"
+  #   end
+  # end
 end
 
-ticket = Ticket.new("Town Hall")
-ticket.discount=0.20
-ticket.price=120
-ticket.date="1999-01-10"
+
+
+th = Ticket.new("Town Hall", "1/1/1")
+cc = Ticket.new("Convention Center", "2/2/2")
+fg = Ticket.new("Fairgrounds", "3/3/3")
+th.discount = 0
+cc.discount = 0
+fg.discount = 0.3
+th.price = 100
+cc.price = 70
+fg.price = 103
+highest = Ticket.most_expensive(th, cc, fg)
+p highest
